3c4b81d507a9516f1aac9b18e41b2bcdac78088e
grancolombiana-ips <abogadoctor@gmail.com>
2026-03-10 18:23:52 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const port = process.env.PORT || 3030;
+const dir = process.cwd();
+
+http.createServer((req, res) => {
+  let file = req.url === '/' ? '/index.html.html' : req.url;
+  const fp = path.join(dir, file);
+  fs.readFile(fp, (err, data) => {
+    if (err) { res.writeHead(404); res.end('Not found: ' + fp); return; }
+    const ext = path.extname(fp);
+    const mime = { '.html': 'text/html', '.css': 'text/css', '.js': 'application/javascript' };
+    res.writeHead(200, { 'Content-Type': mime[ext] || 'text/plain' });
+    res.end(data);
+  });
+}).listen(port, () => console.log('Serving from: ' + dir + ' on port ' + port));
